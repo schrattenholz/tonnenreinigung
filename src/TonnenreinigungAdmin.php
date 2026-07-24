d3e9947fd0532d7dc06f70ceb5a7edbbb20c3536
schrattenholz <46528228+schrattenholz@users.noreply.github.com>
2026-07-24 10:51:33 +0200

    Add a trash-bin icon for the Tonnenreinigung CMS menu entry
    Uses the CMS admin's built-in icon font (font-icon-trash-bin) instead
    of ModelAdmin's generic database icon default.

--- src/TonnenreinigungAdmin.php
+++ src/TonnenreinigungAdmin.php
@@ -9,6 +9,8 @@ class TonnenreinigungAdmin extends ModelAdmin
 
     private static $menu_title = 'Tonnenreinigung';
 
+    private static $menu_icon_class = 'font-icon-trash-bin';
+
     private static $url_segment = 'tonnenreinigung';
 
     private static $managed_models = [
